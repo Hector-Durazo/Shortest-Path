e0efca77fa932f673dbd792082716dbc121536bf
Hector Durazo <hectordurazo1@gmail.com>
2022-03-23 21:01:54 -0700

    First commit

--- ShortestPath.py
+++ ShortestPath.py
@@ -1,30 +1,99 @@
-
 import pygame
-
+ 
+# Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 RED = (255, 0, 0)
-
+ 
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
+ 
+# This sets the margin between each cell
+MARGIN = 5
+startStopCount = 0
+startStopLocation = []
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+ 
+# Initialize pygame
 pygame.init()
-
-width = 30
-pxlWidth = (25 * width) + 5
-size = (pxlWidth, pxlWidth)
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("A* Shortest Path")
-
+ 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [255, 255]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+ 
+# Set title of screen
+pygame.display.set_caption("Array Backed Grid")
+ 
+# Loop until the user clicks the close button.
 done = False
+ 
+# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-
-#	Main Program loop
+ 
+# -------- Main Program Loop -----------
 while not done:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
-	screen.fill(WHITE)
-	pygame.display.flip()
-	clock.tick(60)
-pygame.quite()
-	
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+
+        #deciding where the start and stop are at
+        elif event.type == pygame.MOUSEBUTTONDOWN and startStopCount < 3:
+            pos = pygame.mouse.get_pos()
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            grid[row][column] = 2
+            startStopCount += 1
+            startStopLocation.append(grid[row][column])
+        
+        #after deciding the start and stop
+        if startStopCount >= 3:
+            #if left mouse button is pressed place barrier
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+                # Set that location to one
+                grid[row][column] = 1
+                print("Click ", pos, "Grid coordinates: ", row, column)
+
+            #if the right mouse button is pressed delete barrier
+            if pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+
+                grid[row][column] = 0
+                print("Click ", pos, "Grid coordinates: ", row, column)
+ 
+    # Set the screen background
+    screen.fill(BLACK)
+ 
+    # Draw the grid
+    for row in range(10):
+        for column in range(10):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = BLACK
+            if grid[row][column] == 0:
+                color = WHITE
+            if grid[row][column] == 2:
+                color = GREEN
+            pygame.draw.rect(screen,color,
+                            [(MARGIN + WIDTH) * column + MARGIN,
+                             (MARGIN + HEIGHT) * row + MARGIN,
+                             WIDTH, HEIGHT])
+    clock.tick(60)
+    pygame.display.flip()
+
+pygame.quit()
